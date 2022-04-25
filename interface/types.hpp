b6f9ea8ee3644232b2ba1a0d67af48f119902666
emgullufsen <ericgullufsen@gmail.com>
2022-04-25 13:45:14 -0800

    added key functions

--- interface/types.hpp
+++ interface/types.hpp
@@ -8,18 +8,31 @@ namespace interface
     struct type_base
     {
         virtual ~type_base() = default;
+	virtual void funky() = 0;
+	int num();
     };
 
     struct type_int :
         type_base
     {
         int data;
+	void funky() override {
+		return;
+	};
+	~type_int(){}
     };
 
     struct type_string :
         type_base
     {
         std::string data;
+	void funky() override {
+		return;
+	}
+	~type_string(){}
     };
 
+}
+int interface::type_base::num() {
+	return 33;
 }
